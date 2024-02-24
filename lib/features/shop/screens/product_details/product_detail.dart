2cb0455d5a2da8c05966aac5f84b48330f3cf6cc
raju-islam <rajuslam39@gmail.com>
2024-02-24 17:32:16 +0600

    product details page working

---
+++ lib/features/shop/screens/product_details/product_detail.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class ProductDetail extends StatelessWidget {
+  const ProductDetail({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          
+        ],
+      ),
+    );
+  }
+}
