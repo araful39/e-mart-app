6f27fd205ad1dc826ee7c3a89ec5ccda5c62a41c
raju-islam <rajuslam39@gmail.com>
2024-03-13 01:00:19 +0600

    RValidator class working now

--- lib/features/authentication/screens/signup/signup_widget/signup_form.dart
+++ lib/features/authentication/screens/signup/signup_widget/signup_form.dart
@@ -2,6 +2,7 @@ import 'package:e_mart/features/authentication/controllers/singin_controller/sin
 import 'package:e_mart/features/authentication/screens/signup/signup_widget/terms_condition_checkbox.dart';
 import 'package:e_mart/utills/constants/sizes.dart';
 import 'package:e_mart/utills/constants/texts.dart';
+import 'package:e_mart/utills/validat/validat.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -23,9 +24,7 @@ class RSingUpForm extends StatelessWidget {
             children: [
               Flexible(
                 child: TextFormField(
-                  validator: (value){
-                    return "Enter First Name";
-                  },
+                  validator: (value)=>RValidator.validateEmptyText("First Name", value),
                   controller: controller.firstName,
                   expands: false,
                   decoration: const InputDecoration(
@@ -38,9 +37,7 @@ class RSingUpForm extends StatelessWidget {
               ),
               Flexible(
                 child: TextFormField(
-                  validator: (value){
-                    return "Enter Last Name";
-                  },
+                  validator: (value)=>RValidator.validateEmptyText("Last Name", value),
                   controller: controller.lastName,
                   expands: false,
                   decoration: const InputDecoration(
@@ -54,9 +51,7 @@ class RSingUpForm extends StatelessWidget {
             height: RSizes.spaceBtwInputFields,
           ),
           TextFormField(
-            validator: (value){
-              return "Enter User Name";
-            },
+            validator: (value)=>RValidator.validateEmptyText("User Name", value),
             controller: controller.userName,
             decoration: const InputDecoration(
                 prefixIcon: Icon(Icons.person), hintText: RTexts.userName),
@@ -65,9 +60,7 @@ class RSingUpForm extends StatelessWidget {
             height: RSizes.spaceBtwInputFields,
           ),
           TextFormField(
-            validator: (value){
-              return "Enter valid Email";
-            },
+            validator: (value)=>RValidator.validateEmail(value),
             controller: controller.email,
             decoration: const InputDecoration(
                 prefixIcon: Icon(Icons.email), hintText: RTexts.email),
@@ -76,9 +69,7 @@ class RSingUpForm extends StatelessWidget {
             height: RSizes.spaceBtwInputFields,
           ),
           TextFormField(
-            validator: (value){
-              return "Enter valid PhoneNumber";
-            },
+            validator: (value) => RValidator.validateEmptyText("Phone Number", value),
             controller: controller.phoneNumber,
             decoration: const InputDecoration(
                 prefixIcon: Icon(Icons.phone), hintText: RTexts.phoneNo),
@@ -87,9 +78,7 @@ class RSingUpForm extends StatelessWidget {
             height: RSizes.spaceBtwInputFields,
           ),
           TextFormField(
-            validator: (value){
-              return "Enter Strong Password";
-            },
+            validator: (value)=>RValidator.validatePassword(value),
             controller: controller.password,
             decoration: const InputDecoration(
                 prefixIcon: Icon(Icons.key), hintText: RTexts.password),
